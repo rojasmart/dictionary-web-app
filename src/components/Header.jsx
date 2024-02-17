c8328286e5c6b95d6e234d4788fa9a0a7689b7c3
binarypsilocybin <rogeriosvaldo@gmail.com>
2024-02-17 13:46:12 +0000

    add icon header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
-import { HStack, Icon, Center, Divider } from "@chakra-ui/react";
-import { EditIcon } from "@chakra-ui/icons";
+import { HStack, Center, Divider } from "@chakra-ui/react";
+import IconHeader from "../../public/images/logo.svg";
 import Toggle from "../components/Toggle";
 import SwitchLang from "../components/SwitchLang";
 
 const Header = () => {
   return (
     <HStack minH={"50px"} pt={10} pb={10} justifyContent={"space-between"}>
-      <Icon as={EditIcon} w={8} h={8} />
+      <img src={IconHeader} />
       <HStack>
         <SwitchLang />
         <Center height="50px">
